7cb38073f8a1cde622fd64f806fe26d29fcb47d8
Christian Bongard <christian.bongard90@gmail.com>
2021-11-12 11:06:00 +0400

    fixed the rounding issue

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :item
   validates :comment, :rating, presence: true
-  validates :comment, length: { minimum: 15 }
   validates :rating,  inclusion: { in: (0..5) }, numericality: true
 end
